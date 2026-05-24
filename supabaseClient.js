b6d17baa7a6699757833d55c9816caa0c6d6a644
Valner <Valnersantana1@gmail.com>
2026-05-24 04:39:18 -0300

    vaai

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,6 @@
 // supabaseClient.js
 // Certifique-se de que o script do supabase está carregado no seu index.html
 const supabaseUrl = 'https://qsdmfayaygufcbglchzi.supabase.co';
-const supabaseKey = 'sb_publishable_f5S-IVCDIQg3CA82BOBbwg_iu68O3Q7'; 
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFzZG1mYXlheWd1ZmNiZ2xjaHppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk1NjQ2MzEsImV4cCI6MjA5NTE0MDYzMX0.YQgqzWAP81QVN3JjOO3hAEYxLntynvPHQitwQO1J7ZM'; 
 
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
