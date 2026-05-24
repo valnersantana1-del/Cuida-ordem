3b3e9032051b77dd1c0b824da68f66c4908ff680
Valner <Valnersantana1@gmail.com>
2026-05-24 03:13:58 -0300

    Teste de novo botão

---
+++ supabaseClient.js
@@ -0,0 +1,6 @@
+// supabaseClient.js
+// Certifique-se de que o script do supabase está carregado no seu index.html
+const supabaseUrl = 'https://qsdmfayaygufcbglchzi.supabase.co';
+const supabaseKey = 'sb_publishable_f5S-IVCDIQg3CA82BOBbwg_iu68O3Q7'; 
+
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
