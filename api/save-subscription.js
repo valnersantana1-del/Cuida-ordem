31317de4dccbde3a6935307ebc713c6bbfb0a4ed
Valner <Valnersantana1@gmail.com>
2026-05-23 22:47:56 -0300

    Correção

--- api/save-subscription.js
+++ api/save-subscription.js
@@ -2,8 +2,8 @@ import { createClient } from '@supabase/supabase-js';
 
 // A Vercel injeta essas variáveis automaticamente graças à integração que você fez
 const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
 
 export default async function handler(req, res) {
